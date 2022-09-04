f58827bafcf879568e885e40ebbb66fc8490cd21
nN1ce <nicevector@yandex.ru>
2022-09-04 10:02:01 +0300

    добавлен негативный тест test_failed_hw в test_HW_lesson_2.py

--- test_HW_lesson_2.py
+++ test_HW_lesson_2.py
@@ -2,9 +2,16 @@ from selene.support.shared import browser
 from selene import be, have
 import pytest
 
+
 # passed_test
 def test_passed_hw(size_window):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('Selene - User-oriented').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
+
+# fails_test
+def test_failed_hw(size_window):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('Selene - User-oriented').press_enter()
+    browser.element('[id="search"]').should(have.text('Java или Python для QA?'))
